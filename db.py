468493333f8992f034ae6398173db3169a397712
Raptorrrr <iqbalalfiansyah544@gmail.com>
2020-10-10 15:48:18 +0700

    berhasil terhubung dan menambah data

--- db.py
+++ db.py
@@ -4,13 +4,16 @@ db = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="",
-    database="Python"
+    database="python"
 )
 
 cursor = db.cursor()
+ID = 18081010123
+Nama = "Nafa"
+Kelahiran = "1999-09-01"
+val = (ID, Nama, Kelahiran)
 sql = "INSERT INTO biodata (Id,Nama,Kelahiran) VALUES (%s,%s,%s)"
-val = ("18081010123", "Iqbal", "2000-09-01")
-
+cursor.execute(sql, val)
 db.commit()
 
 print("{} data ditambahkan".format(cursor.rowcount))
